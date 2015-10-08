a76afabed3d1916ba7e6fa7af9217395c398de47
Hugo Fonseca <hugo@deziworks.com>
2015-10-08 19:20:45 +0100

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ubiwhereApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ubiwhereApp
+ */
+angular.module('ubiwhereApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
